88c062923e3d0543b52840cd9172299c0215c225
ujjawaltyagii <ujjawal2002tyagi@gmail.com>
2023-02-21 22:28:41 +0530

    heading bug resolved & removed logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+         stencil: ["Allerta Stencil", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 };
