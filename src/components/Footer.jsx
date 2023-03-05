f512040c265501d24437619788de96a080f695d8
RotrixX <ncusnir43@gmail.com>
2023-03-05 16:03:43 +0000

    feat: migrate to react

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+const Footer = () => {
+    return (
+        <footer aria-label="Site Footer">
+            <div className="max-w-screen-xl  pb-8 mx-auto sm:px-6">
+                <div className="pt-8 mt-16 border-t flex justify-center items-center border-gray-100 dark:border-gray-800">
+                    <div className="flex items-center justify-start ml-4 space-x-4">
+                        <a href="https://github.com/priyankarpal/ProjectsHut" title="Github" target="_blank">
+                            <i className="fa-brands fa-github fa-xl" />
+                        </a>
+                        <a href="https://twitter.com/Priyankarpal" title="Twitter" target="_blank">
+                            <i className="fa-brands fa-twitter fa-xl" />
+                        </a>
+                    </div>
+                    
+                    <div className="flex items-center justify-center flex-1">
+                        Handcrafted By Students ❤️🚀
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
