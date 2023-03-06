1780e9982364580006533cc6b45c1bcd26024fda
priyankarpal <priyankarpalpapai2005@gmail.com>
2023-03-06 13:55:21 +0530

    fix:new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,30 +2,41 @@ const Navbar = () => {
   return (
     <nav
       aria-label="Site Nav"
-      className="mx-auto flex max-w-3xl items-center justify-between p-4"
+      class="mx-auto flex max-w-3xl items-center justify-between p-4"
     >
       <a
         href="/"
-        className="inline-flex h-10 w-10 items-center justify-center rounded-lg bg-gray-100"
+        class="inline-flex h-10 w-10 items-center justify-center rounded-lg text-white font-bold text-[1.3rem] "
       >
-        🔥
+        ph.
       </a>
-      <ul className="flex items-center gap-2 text-sm font-medium text-white ">
-        <li className="hover:text-primary">
-          <a
-            className="rounded-lg px-3 py-2 "
-            href="https://github.com/priyankarpal/ProjectsHut/blob/main/contributing.md"
-            target="_blank"
-          >
-            Docs
+
+      <ul class="flex items-center gap-2  text-[1.3rem] text-white">
+        <li class="hidden lg:block">
+          <a class="rounded-lg px-3 py-2" href="/">
+            {" "}
+            Home{" "}
           </a>
         </li>
 
-        <li className="hover:text-primary">
+        <li>
           <a
-            className="inline-flex items-center rounded-lg px-3 py-2"
-            href="https://github.com/priyankarpal/ProjectsHut"
+            class="rounded-lg px-3 py-2"
+            href="https://github.com/priyankarpal/ProjectsHut/blob/main/contributing.md"
             target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Docs{" "}
+          </a>
+        </li>
+
+        <li>
+          <a
+            class="inline-flex items-center gap-2 rounded-lg px-3 py-2"
+            href="https://github.com/priyankarpal/ProjectsHut/"
+            target="_blank"
+            rel="noreferrer"
           >
             GitHub
             <svg
@@ -33,12 +44,12 @@ const Navbar = () => {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              className="ml-1.5 h-4 w-4"
+              class="h-4 w-4"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
                 d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
               ></path>
             </svg>
