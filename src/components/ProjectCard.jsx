e6c13029e739ee5bd48c73f00dc25c8948bc448d
Abhishek k <bobbyyyyy.16@gmail.com>
2023-03-06 04:30:58 +0000

    #54 has be resolved

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,7 +2,7 @@ const ProjectCard = ({ gh, name, link, title, description, tech }) => {
   return (
     <div className="flex flex-col border shadow-sm rounded-xl p-4 md:p-5 dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7] transition-all duration-200  hover:scale-105  cursor-pointer">
       <div className="flex justify-between">
-        <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+        <h3 className="text-lg font-bold text-white-800 dark:text-white">
           {title}
         </h3>
 
@@ -22,7 +22,7 @@ const ProjectCard = ({ gh, name, link, title, description, tech }) => {
           </p>
         ))}
       </p>
-      <p className="mt-2 text-gray-800 dark:text-gray-400">{description}</p>
+      <p className="mt-2 text-white-800 dark:text-white-400">{description}</p>
       <a
         className="mt-3 inline-flex items-center gap-2  text-sm font-medium p-3 justify-center bg-primary rounded text-white   xl:text-[1rem] md:text-[0.8rem]"
         href={link}
