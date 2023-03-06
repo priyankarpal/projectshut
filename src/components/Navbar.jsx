b19437dcd49eff8637da290396e02c57b6c0ba76
priyankarpal <priyankarpalpapai2005@gmail.com>
2023-03-06 16:22:41 +0530

    Added className 😀

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,38 +2,36 @@ const Navbar = () => {
   return (
     <nav
       aria-label="Site Nav"
-      class="mx-auto flex max-w-3xl items-center justify-between p-4"
+      className="mx-auto flex max-w-3xl items-center justify-between p-4"
     >
       <a
         href="/"
-        class="inline-flex h-10 w-10 items-center justify-center rounded-lg text-white font-bold text-[1.3rem] "
+        className="inline-flex h-10 w-10 items-center justify-center rounded-lg text-white font-bold text-[1.3rem] "
       >
         ph.
       </a>
 
-      <ul class="flex items-center gap-2  text-[1.3rem] text-white">
-        <li class="hidden lg:block">
-          <a class="rounded-lg px-3 py-2" href="/">
-            {" "}
-            Home{" "}
+      <ul className="flex items-center gap-2  text-[1.3rem] text-white">
+        <li className="hidden lg:block">
+          <a className="rounded-lg px-3 py-2" href="/">
+            Home
           </a>
         </li>
 
         <li>
           <a
-            class="rounded-lg px-3 py-2"
+            className="rounded-lg px-3 py-2"
             href="https://github.com/priyankarpal/ProjectsHut/blob/main/contributing.md"
             target="_blank"
             rel="noreferrer"
           >
-            {" "}
-            Docs{" "}
+            Docs
           </a>
         </li>
 
         <li>
           <a
-            class="inline-flex items-center gap-2 rounded-lg px-3 py-2"
+            className="inline-flex items-center gap-2 rounded-lg px-3 py-2"
             href="https://github.com/priyankarpal/ProjectsHut/"
             target="_blank"
             rel="noreferrer"
