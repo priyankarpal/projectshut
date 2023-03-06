1b25b0a08c6765fd1f8a1d0609dae9fd497b22b5
priyankarpal <priyankarpalpapai2005@gmail.com>
2023-03-07 00:00:27 +0530

    fix: added best practice

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,12 +44,12 @@ const Navbar = () => {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
-              class="h-4 w-4"
+              className="h-4 w-4"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
               ></path>
             </svg>
