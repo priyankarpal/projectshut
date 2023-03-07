2a8b7a94ec4c412781a6d4d2f11f09c01c3004e5
priyankarpal <priyankarpalpapai2005@gmail.com>
2023-03-07 12:53:40 +0530

    Added /

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="projects" element={<Projects />} />
+        <Route exact path="/projects" element={<Projects />} />
         <Route exact path="/*" element={<Error />} />
       </Routes>
       <Footer />
