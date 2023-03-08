e571e514d380da27dfda55a7933ce30047525cf9
priyankarpal <priyankarpalpapai2005@gmail.com>
2023-03-08 12:39:33 +0530

    Added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App";
 import "./CSS/index.css";
 import { HashRouter } from "react-router-dom";
 import { inject } from "@vercel/analytics";
-
 inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
