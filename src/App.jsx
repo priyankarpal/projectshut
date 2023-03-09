ad7efcecd05ebdb84edcb70e726516e53e50b57c
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-03-09 23:49:22 +0530

    Added browserrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import { Navbar, Footer } from "./components";
-import { ProjectsPage, HomePage } from "./pages";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Navbar, Footer } from "./components"
+import { ProjectsPage, HomePage } from "./pages"
+import { Route, Routes, Navigate } from "react-router-dom"
 
 function App() {
   return (
     <div className=" text-white">
       <Navbar />
+
       <Routes>
         <Route exact path="/" element={<HomePage />} />
+        <Route index element={<HomePage />} />
         <Route path="ProjectsPage" element={<ProjectsPage />} />
         <Route path="/*" element={<Navigate to={"/"} />} />
       </Routes>
+
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./CSS/index.css";
-import { HashRouter } from "react-router-dom";
-import { inject } from "@vercel/analytics";
-inject();
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./CSS/index.css"
+import { BrowserRouter } from "react-router-dom"
+import { inject } from "@vercel/analytics"
+inject()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
-);
+  </BrowserRouter>,
+)
