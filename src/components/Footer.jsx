f693e6fa4e6a56a0f8f1010c3aa744574146f1f1
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-03-20 15:56:00 +0530

    feat: added share link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -18,16 +18,14 @@ const Footer = () => {
       </p>
 
       <ul className="flex items-center gap-2  text-white text-[1rem]">
-        <li className="lg:block  "></li>
-
-        <li>
+        <li className="pb-5">
           <a
-            href="https://twitter.com/priyankarpal"
+            href="https://twitter.com/intent/tweet?text=Exciting%20news%21%F0%9F%A5%B3%20I%20just%20discovered%20an%20awesome%20open-source%20project%20called%20ProjectsHut.%20If%20you%27re%20looking%20to%20contribute%20your%20first%20code%20or%20simply%20want%20to%20explore%20some%20cool%20programming%20projects%2C%20you%20should%20definitely%20check%20it%20out.%20https%3A%2F%2Fgithub.com%2Fpriyankarpal%2FProjectsHut"
             target={"_blank"}
             rel={"noreferrer"}
             className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
           >
-            Follow on Twitter
+            Tweet on Twitter
           </a>
         </li>
       </ul>
