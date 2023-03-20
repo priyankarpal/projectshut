55ea3b6e777fcc71959ea1200549e1c36d2fddba
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-03-21 00:24:36 +0530

    chore: changed text to share on twitter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
             rel={"noreferrer"}
             className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
           >
-            Tweet on Twitter
+            Share on Twitter
           </a>
         </li>
       </ul>
