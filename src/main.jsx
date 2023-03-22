098dc2c2d4d0b353cd625c88e1685b962fa7e1e8
Jason Dsouza <74404894+JasonDsouza212@users.noreply.github.com>
2023-03-22 17:33:44 +0530

    Changed from Hashrouter to BrowserRouter
    Due to the unnecessary # symbol in the website navigation caused by HashRouter, it has been replaced with BrowserRouter to resolve the issue.

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./CSS/index.css"
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import { inject } from "@vercel/analytics"
 inject()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>
 )
+
