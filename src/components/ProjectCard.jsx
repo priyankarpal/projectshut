f4ebf1f16ee4ef9558f9febe1406e726b0de774c
Rupali Haldiya <78981177+rupali-codes@users.noreply.github.com>
2023-03-31 20:20:12 +0530

    fix: alignment of GitHub Link button (#126)

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,20 +1,27 @@
 const ProjectCard = ({ gh, link, title, description, tech }) => {
   return (
     <div className="flex flex-col border shadow-sm rounded-xl p-4 md:p-5 dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7] transition-all duration-200  hover:scale-105  cursor-pointer">
-      <div className="flex justify-between">
-        <h3 className="text-lg font-bold text-white-800 dark:text-white">{title}</h3>
+      <div className="flex flex-col justify-between h-full">
+        <div>
+          <div className="flex justify-between">
+            <h3 className="text-lg font-bold text-white-800 dark:text-white">{title}</h3>
 
-        <img src={`https://github.com/${gh}.png`} className="h-10 w-10 rounded-full" alt={`${gh}'s github profile`} />
-      </div>
-      <div className="mt-1  text-gray-500 dark:text-gray-500 flex flex-row overflow-auto scrollbar items-center">
-        {tech.map((tag, i) => (
-          <p className="bg-red-500 rounded-full text-white pl-[.5rem] pr-[.5rem] m-[.2rem] text-[.8rem]" key={i}>
-            {tag}
-          </p>
-        ))}
-      </div>
-      <p className="mt-2 text-white-800 dark:text-white-400">{description}</p>
-      <a
+            <img
+              src={`https://github.com/${gh}.png`}
+              className="h-10 w-10 rounded-full"
+              alt={`${gh}'s github profile`}
+            />
+          </div>
+          <div className="mt-1  text-gray-500 dark:text-gray-500 flex flex-row overflow-auto scrollbar items-center">
+            {tech.map((tag, i) => (
+              <p className="bg-red-500 rounded-full text-white pl-[.5rem] pr-[.5rem] m-[.2rem] text-[.8rem]" key={i}>
+                {tag}
+              </p>
+            ))}
+          </div>
+          <p className="mt-2 text-white-800 dark:text-white-400">{description}</p>
+        </div>
+        <a
         className="mt-3 inline-flex items-center gap-2  text-sm font-medium p-3 justify-center bg-primary rounded text-white   xl:text-[1rem] md:text-[0.8rem]"
         href={link}
         target="_blank"
@@ -22,6 +29,8 @@ const ProjectCard = ({ gh, link, title, description, tech }) => {
       >
         GitHub Link <span aria-hidden="true">→</span>
       </a>
+      </div>
+
     </div>
   )
 }
