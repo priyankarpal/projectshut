ebee4960c00db7de5cb623a64357d46a18502772
Ryan Lusby <51033557+RLTek@users.noreply.github.com>
2023-04-01 10:57:07 -0400

    Added hover effect to github links using tailwind (#131)

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -22,7 +22,7 @@ const ProjectCard = ({ gh, link, title, description, tech }) => {
           <p className="mt-2 text-white-800 dark:text-white-400">{description}</p>
         </div>
         <a
-          className="mt-3 inline-flex items-center gap-2  text-sm font-medium p-3 justify-center bg-primary rounded text-white   xl:text-[1rem] md:text-[0.8rem]"
+          className="mt-3 inline-flex items-center gap-2  text-sm font-medium p-3 justify-center bg-primary rounded text-white   xl:text-[1rem] md:text-[0.8rem] hover:scale-105 hover:bg-purple-700"
           href={link}
           target="_blank"
           rel="noreferrer"
