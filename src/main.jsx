a96bd1fd05fde4f38b305a73ef0e096eba64b823
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-04-11 20:16:49 +0530

    chore: routing added

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./CSS/index.css"
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import { inject } from "@vercel/analytics"
 inject()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
 )
