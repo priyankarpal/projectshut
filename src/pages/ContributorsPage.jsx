33ae0ce5655051586c7c5bd26178098a5ba9870d
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-04-13 15:02:53 +0530

    feat: added contributors profile using GitHub API #190

--- src/pages/ContributorsPage.jsx
+++ src/pages/ContributorsPage.jsx
@@ -1,12 +1,13 @@
 import React from "react"
+import Contributors from "../components/ContributorsCard"
 
 function ContriButorsPage() {
   return (
     <main className=" my-8 min-h-[65.75vh] max-w-6xl w-11/12 mx-auto sm:min-h-[73vh] lg:min-h-[78vh]">
-      {/* As the number of cards may change, it is important to give a min-height to 'main' */}
-      <h1 className="text-[3.5rem] font-bold  text-center">
+      <h1 className=" text-[1.5rem] md:text-[3rem] pb-5 font-bold  text-center">
         Our <span className="text-primary">Contributors </span>
       </h1>
+      <Contributors owner="priyankarpal" repo="ProjectsHut" />
     </main>
   )
 }
