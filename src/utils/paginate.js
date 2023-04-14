7d096d15a1ea7db6f6b8a3cf828c7b5126309657
Sayantan Manna <sayantanmanna06@gmail.com>
2023-04-14 18:51:05 +0530

    Implemented Pagination (#193)
    ## Description
    
    As described in #178 , a pagination system has been added. Also, line
    clamp is added to Product Card title.
    
    ## Screenshots
    
    <!-- PLease add screenshots -->
    
    ![Pagination_projectHub](https://user-images.githubusercontent.com/92118155/231947409-147649fa-f1b5-49f9-87ae-b5acec919161.JPG)
    
    
    ## Checklist
    
    Please review and check off the following items before submitting your
    PR:
    
     <!-- If yes then add 'x' into '[ ]'  -->
    
    - [x] I have tested my changes to ensure they work as expected
    - [x] I have included documentation for any new features or changes
    - [x] I have formatted my code according to the project's style
    guidelines
    - [x] I have written tests for any new features or changes
    - [x] I have run the project's test suite and verified that all tests
    pass
    - [x] I have updated the README and any other relevant documentation
    - [x] I have checked for any spelling or grammatical errors
    
    ## Additional Information
    
    In case of tech-labels in Product-Card, usage of line-clamp property
    shows inappropriate behavior (as shown below) . Therefore, JS is used
    for maintaining the length of the line.
    
    ![Disturbance_lineClamp](https://user-images.githubusercontent.com/92118155/231946991-4fb542cd-c79e-4fe6-b884-f95646f21fbd.JPG)

---
+++ src/utils/paginate.js
@@ -0,0 +1,11 @@
+export const paginate = (followers) => {
+  const itemsPerPage = 9
+  const pages = Math.ceil(followers.length / itemsPerPage)
+
+  const newFollowers = Array.from({ length: pages }, (item, index) => {
+    let start = itemsPerPage * index
+    return followers.slice(start, start + itemsPerPage)
+  })
+
+  return newFollowers
+}
