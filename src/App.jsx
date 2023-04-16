f556115b6302133d6bce8cbb261b4afb54456565
Saurav Pant <pantsaurav005@gmail.com>
2023-04-16 21:27:04 +0530

    Splash Screen Added (#205)
    https://user-images.githubusercontent.com/103263909/232324460-73b53426-9bda-4ee9-af25-409241346b81.mp4

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
+import React, { useState, useEffect } from "react"
 import { Navbar, Footer } from "./components"
 import { ProjectsPage, HomePage, ContriButorsPage, AddYourProjectsGuide } from "./pages"
-import { Route, Routes, Navigate } from "react-router-dom"
+import { Route, Routes, Navigate, useLocation } from "react-router-dom"
+import SplashScreen from "./components/SplashScreen"
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const location = useLocation()
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 1000)
+  }, [])
+
+  const showSplashScreen = location.pathname === "/"
+
   return (
     <div className=" text-white font-mono">
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/ProjectsPage" element={<ProjectsPage />} />
-        <Route exact path="/ContributorsPage" element={<ContriButorsPage />} />
-        <Route exact path="/AddYourProjectsGuide" element={<AddYourProjectsGuide />} />
-        <Route exact path="/*" element={<Navigate to={"/"} />} />
-      </Routes>
-      <Footer />
+      {showSplashScreen && isLoading ? (
+        <SplashScreen />
+      ) : (
+        <>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/ProjectsPage" element={<ProjectsPage />} />
+            <Route exact path="/ContributorsPage" element={<ContriButorsPage />} />
+            <Route exact path="/AddYourProjectsGuide" element={<AddYourProjectsGuide />} />
+            <Route exact path="/*" element={<Navigate to={"/"} />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </div>
   )
 }
