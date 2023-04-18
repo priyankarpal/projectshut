d2e57ce48cb1836ee99d88b6790bc599e941d623
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-04-19 00:07:31 +0530

    chore: removed vercel analytics code #207

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./CSS/index.css"
 import { BrowserRouter } from "react-router-dom"
-import { inject } from "@vercel/analytics"
-inject()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
