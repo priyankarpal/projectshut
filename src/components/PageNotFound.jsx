74beed50bddc89f7bcd73b5edacf0f9323098f92
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-04-18 10:01:53 +0530

    chore: added padding

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const PageNotFound = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center text-white bg-[#000513]">
+    <div className="min-h-screen flex flex-col justify-center items-center text-white bg-[#000513] p-5">
       <h1 className="text-6xl font-bold mb-8 animate-bounce">404 - Page Not Found</h1>
       <p className="text-2xl mb-8">
         Oops! Looks like you followed a bad link. If you think this is a mistake, please <Link to="/">contact us</Link>.
