f0ec0e904fa5686acaa63a46e304d73bccd012b5
unknown <hrppanchal27@gmail.com>
2023-04-22 15:07:45 +0530

    code for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,74 @@
-import { Link } from "react-router-dom"
+/*eslint-disable*/
+import { Link, NavLink } from "react-router-dom"
 import { FaGithub } from "react-icons/fa"
+import "../CSS/navbar.css"
 
 const Navbar = () => {
   return (
-    <nav aria-label="Site Nav" className="mx-auto flex max-w-3xl items-center justify-between p-4 ">
-      <Link
-        to="/"
-        className="inline-flex h-10 items-center justify-center rounded-lg text-white font-extrabold text-[2rem] "
-      >
-        Ph <span className="text-red-500">.</span>
-      </Link>
-
-      <ul className="flex items-center gap-5  text-white text-[1rem]">
-        <li>
-          <a
-            href="https://github.com/priyankarpal/ProjectsHut"
-            target={"_blank"}
-            rel={"noreferrer"}
-            className="text-[1.5rem] "
-          >
-            <FaGithub />
-          </a>
-        </li>
-        <li>
-          <Link
-            to="/ContributorsPage"
-            className="inline-block rounded-md border border-transparent bg-[#FFFFFF] py-2 px-3 text-center font-bold text-black hover:bg-[##FFFFFF]"
-          >
-            Contributors <span aria-hidden="true">→</span>
+    // <div>
+    <nav aria-label="Site Nav" className=" mx-auto  justify p-4 w-full bg-gray-900" style={{}}>
+      <div className="nav-item-container">
+        {/* Logo for project Hunt */}
+        <div className="item-navbar" id="logo-ph">
+          <Link to="/" className="inline-flex h-10 items-center rounded-lg text-white font-extrabold text-[2rem] ">
+            Ph <span className="text-red-500">.</span>
           </Link>
-        </li>
-      </ul>
+        </div>
+        {/* Main element of navbar */}
+        <div className="item-navbar hidden md:block" id="elements-of-navbar">
+          <ul className="flex items-center gap-5 text-white text-[1rem]">
+            <li>
+              <NavLink to="/" className="inline-block py-2 px-3 text-center font-bold text-white hover:bg-[##FFFFFF]">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/ProjectsPage"
+                className="inline-block py-2 px-3 text-center font-bold text-white hover:bg-[##FFFFFF]"
+              >
+                Project Page
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/AddYourProjectsGuide"
+                className="inline-block py-2 px-3 text-center font-bold text-white hover:bg-[##FFFFFF]"
+              >
+                Guide
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/ContributorsPage"
+                className="inline-block py-2 px-3 text-center font-bold text-white hover:bg-[##FFFFFF]"
+              >
+                Our Contributers
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        {/* Additional elemnt of navbar */}
+        <div className="item-navbar" id="additional-elm">
+          <ul className="flex items-center gap-5 text-white text-[1rem]">
+            <li>
+              <a
+                href="https://github.com/priyankarpal/ProjectsHut"
+                target={"_blank"}
+                rel={"noreferrer"}
+                className="inline-flex h-10 items-center rounded-lg text-white font-extrabold text-[2rem] "
+              >
+                <FaGithub />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
+    // </div>
   )
 }
 
 export default Navbar
+
+// mx-auto
