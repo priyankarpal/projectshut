a127175c3f272b5fbf1e921996fe7112bfef80cc
Shivam KUmar <shivambharadwaj822@gmail.com>
2023-04-22 19:12:32 +0530

    I have removed the twitter logo and share on twitter button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,18 +12,10 @@ const Footer = () => {
             rel="noreferrer"
             className="text-primary"
           >
-            MIT License
-          </a>
-          <span> &copy; {new Date().getFullYear()} - Open Source</span>
-        </p>
-        <a
-          href="https://twitter.com/intent/tweet?text=Exciting%20news%21%F0%9F%A5%B3%20I%20just%20discovered%20an%20awesome%20open-source%20project%20called%20ProjectsHut.%20If%20you%27re%20looking%20to%20contribute%20your%20first%20code%20or%20simply%20want%20to%20explore%20some%20cool%20programming%20projects%2C%20you%20should%20definitely%20check%20it%20out.%20https%3A%2F%2Fgithub.com%2Fpriyankarpal%2FProjectsHut"
-          target={"_blank"}
-          rel={"noreferrer"}
-          className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-        >
-          Share on Twitter
+          MIT License
         </a>
+        <span> &copy; {new Date().getFullYear()} - Open Source</span>
+        </p>
       </div>
     </footer>
   )
