86b0e9a16596c90cc53e841fe1ffde553653cd96
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-04-23 14:20:40 +0530

    chore: footer center #244

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,18 @@ import React from "react"
 const Footer = () => {
   return (
     <footer aria-label="Site Footer">
-      <div className="mx-auto p-4 flex flex-wrap gap-5 justify-center max-w-3xl items-center sm:justify-between">
-        <p className="text-center">
-          Under{" "}
-          <a
-            href="https://github.com/priyankarpal/ProjectsHut/blob/main/LICENSE"
-            target={"_blank"}
-            rel="noreferrer"
-            className="text-primary"
-          >
-            MIT License
-          </a>
-          <span> &copy; {new Date().getFullYear()} - Open Source</span>
-        </p>
-      </div>
+      <p className="text-center p-5">
+        Under{" "}
+        <a
+          href="https://github.com/priyankarpal/ProjectsHut/blob/main/LICENSE"
+          target={"_blank"}
+          rel="noreferrer"
+          className="text-primary"
+        >
+          MIT License
+        </a>
+        <span> &copy; {new Date().getFullYear()} - Open Source</span>
+      </p>
     </footer>
   )
 }
