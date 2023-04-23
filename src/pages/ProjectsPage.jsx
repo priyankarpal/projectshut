91aa28eba9d265fd92dfdaf5ecb4aef0737797f2
Ahtaxam <64573767+Ahtaxam@users.noreply.github.com>
2023-04-23 14:01:32 +0500

    Add Search Functionality (#241)
    ## Description
    
    <!-- Please describe the changes you made to the Projectshut Open Source
    website. What features did you add or modify? What bug did you fix? -->
    closes #223 
    
    ## Screenshots
    
    <!-- PLease add screenshots -->
    
    ## Related Issue
    
    <!-- Please list any related issues that your PR addresses. For example,
    if you fixed a bug, please reference the issue number that describes the
    bug.
     -->
    
    ## Checklist
    
    Please review and check off the following items before submitting your
    PR:
    
     <!-- If yes then add 'x' into '[ ]'  -->
    
    - [x] I have tested my changes to ensure they work as expected
    - [x] I have included documentation for any new features or changes
    - [x] I have formatted my code according to the project's style
    guidelines
    - [ ] I have written tests for any new features or changes
    - [x] I have run the project's test suite and verified that all tests
    pass
    - [ ] I have updated the README and any other relevant documentation
    - [x] I have checked for any spelling or grammatical errors
    
    ## Additional Information
    
    <!--
    Is there anything else you would like to include about your PR? This
    could include information about how to test your changes or any
    potential issues that may arise.
    
    Thank you for contributing to the Projectshut Open Source website!
     -->
    
    ---------
    
    Co-authored-by: Priyankar Pal <88102392+priyankarpal@users.noreply.github.com>

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,15 +1,36 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { ProjectCard } from "../components"
 import projects from "../DB/projects.json"
+import techStack from "../utils/techStack"
 import { paginate } from "../utils/paginate"
+import { Button } from "@mui/material"
 
 const paginatedArr = paginate(projects)
 
 const ProjectsPage = () => {
   const [page, setPage] = useState(0)
+  const [currentItems, setItems] = useState([])
+  const [selectedButton, setSelectedButton] = useState(null)
 
-  const currentItems = paginatedArr[page]
+  // this useEffect is for when user click on pagination button then render only that page projects
+  useEffect(() => {
+    const data = paginatedArr[page]
+    setItems(data)
+  }, [page])
 
+  // this useEffect is for when user clear the filter (double click) then render only that page projects
+  useEffect(() => {
+    if (selectedButton === null) {
+      setItems(paginatedArr[page])
+      return
+    }
+  }, [selectedButton])
+
+  // this function will filter project based on selected technology and set the state of items
+  const handleQuery = (index) => {
+    setSelectedButton((prev) => (prev === index ? null : index))
+    setItems(projects.filter((obj) => obj.tech.includes(techStack[index])))
+  }
   const prevPage = () => {
     if (page - 1 < 0) {
       setPage(paginatedArr.length - 1)
@@ -37,6 +58,19 @@ const ProjectsPage = () => {
           Check documentation <span aria-hidden="true">→</span>
         </a>
       </p>
+      <p className="flex justify-center mt-6 text-primary text-center ">Sort project based on technology</p>
+      <div className="flex flex-wrap justify-start md:justify-center m-4 gap-2 ">
+        {techStack.map((tech, index) => (
+          <Button
+            key={index}
+            onClick={() => handleQuery(index)}
+            variant={selectedButton === index ? "contained" : "outlined"}
+            className="bg-primary hover:bg-slate-200"
+          >
+            <span className={selectedButton == index ? "text-white" : "text-primary"}> {tech.toLowerCase()}</span>
+          </Button>
+        ))}
+      </div>
 
       {/* As the number of cards may change, it is important to give a min-height to 'section' */}
       <section className="my-7 min-h-[34vh] sm:grid sm:grid-cols-2 sm:auto-rows-min sm:gap-x-2 sm:gap-y-4 sm:justify-items-center sm:items-center sm:min-h-[37vh] md:gap-x-3 md:min-h-[50vh] lg:grid-cols-3 lg:min-h-[60vh] xl:min-h-[70vh]">
@@ -44,26 +78,30 @@ const ProjectsPage = () => {
           <ProjectCard gh={project["gh-username"]} {...project} key={i} />
         ))}
       </section>
-      <div className=" py-5 flex gap-2 flex-wrap justify-center text-black ">
-        <button type="button" className="bg-white px-3 py-1 hover:bg-slate-200 rounded-md " onClick={prevPage}>
-          Prev
-        </button>
-        {paginatedArr.map((ele, ind) => {
-          return (
-            <button
-              type="button"
-              className={`bg-white px-3 py-1 hover:bg-slate-200 rounded-md ${page === ind ? "text-primary" : null}`}
-              onClick={() => setPage(ind)}
-              key={ind}
-            >
-              {ind + 1}
-            </button>
-          )
-        })}
-        <button type="button" className="bg-white px-3 py-1 rounded-md hover:bg-slate-200" onClick={nextPage}>
-          Next
-        </button>
-      </div>
+
+      {/* when user apply filter then show specific projects and hide prev and next page */}
+      {selectedButton === null && (
+        <div className=" py-5 flex gap-2 flex-wrap justify-center text-black ">
+          <button type="button" className="bg-white px-3 py-1 hover:bg-slate-200 rounded-md " onClick={prevPage}>
+            Prev
+          </button>
+          {paginatedArr.map((ele, ind) => {
+            return (
+              <button
+                type="button"
+                className={`bg-white px-3 py-1 hover:bg-slate-200 rounded-md ${page === ind ? "text-primary" : null}`}
+                onClick={() => setPage(ind)}
+                key={ind}
+              >
+                {ind + 1}
+              </button>
+            )
+          })}
+          <button type="button" className="bg-white px-3 py-1 rounded-md hover:bg-slate-200" onClick={nextPage}>
+            Next
+          </button>
+        </div>
+      )}
     </main>
   )
 }
---
+++ src/utils/techStack.js
@@ -0,0 +1,1 @@
+export default ["React", "NodeJs", "CSS", "HTML", "NextJS", "JavaScript", "Python"]
