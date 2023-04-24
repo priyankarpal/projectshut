924195d00bc4adef9aff6994db4ff448be972c16
Shivam Kumar <shivambharadwaj822@gmail.com>
2023-04-25 01:03:41 +0530

    feat: footer redesigned #235 (#266)
    @priyankarpal  issue no #235
    
    ---------
    
    Co-authored-by: Priyankar Pal <88102392+priyankarpal@users.noreply.github.com>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,27 @@
 import React from "react"
+import { FaDiscord, FaGithub } from "react-icons/fa"
 
 export default function Footer() {
   return (
-    <footer aria-label="Site Footer">
-      <p className="text-center p-5">
-        Under{" "}
-        <a
-          href="https://github.com/priyankarpal/ProjectsHut/blob/main/LICENSE"
-          target={"_blank"}
-          rel="noreferrer"
-          className="text-primary"
-        >
-          MIT License
-        </a>
-        <span> &copy; {new Date().getFullYear()} - Open Source</span>
-      </p>
+    <footer aria-label="Site Footer" className="bg-white dark:bg-gray-900">
+      <div className="max-w-screen-xl px-4 pt-16 pb-8 mx-auto sm:px-6 lg:px-8 lg:pt-24">
+        <div className="text-center">
+          <h2 className="text-3xl font-extrabold text-white sm:text-5xl mb-4">Built for collaboration 🤝</h2>
+          <p className="max-w-sm mx-auto mt-4 text-gray-400">
+            ProjectsHut is an open-source project. Feel free to contribute and help us make this project even better
+          </p>
+        </div>
+
+        <div className="pt-8 mt-16 border-t flex gap-3 border-gray-100 dark:border-gray-800   sm:justify-center lg:mt-24">
+          <a
+            href="https://github.com/priyankarpal/ProjectsHut/blob/main/LICENSE"
+            target="_blank"
+            className="text-white"
+          >
+            Under MIT License
+          </a>
+        </div>
+      </div>
     </footer>
   )
 }
