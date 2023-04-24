144e905937d0aad2fdeba1c434a4aa75ddf2f10d
Priyankar Pal <88102392+priyankarpal@users.noreply.github.com>
2023-04-25 00:24:12 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer aria-label="Site Footer" className="bg-white dark:bg-gray-900">
       <div className="max-w-screen-xl px-4 pt-16 pb-8 mx-auto sm:px-6 lg:px-8 lg:pt-24">
         <div className="text-center">
-          <h2 className="text-3xl font-extrabold text-white sm:text-5xl">proudly open source</h2>
+          <h2 className="text-3xl font-extrabold text-white sm:text-5xl">Built for collaboration 🤝🏼</h2>
 
           <p className="max-w-sm mx-auto mt-4 text-gray-400">
             ProjectsHut is an open source project. Feel free to contribute to the project.
