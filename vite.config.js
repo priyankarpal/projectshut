498332c4726cb17b98665a90cd90a7c35a9cb20b
AhorIsaac <ahorisaac.ai@gmail.com>
2023-05-12 12:28:08 +0100

    chore: shorten js imports with aliases

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+import * as path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,8 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
   },
 })
