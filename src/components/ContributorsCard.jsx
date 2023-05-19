451f7f84b2b9c1cea148ed004e069b4228e6a46c
Shivam Kumar <shivkumar386112@gmail.com>
2023-05-20 02:15:16 +0530

    removed node_fetch module which was causing error while deployment

--- src/components/ContributorsCard.jsx
+++ src/components/ContributorsCard.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react"
 import process from "process"
-import fetch from "node-fetch"
 
 function Contributors({ owner, repo }) {
   const [contributors, setContributors] = useState([])
