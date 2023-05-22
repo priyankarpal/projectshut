3532b15772f3109428a0f595b135b5d91b764e25
Amitav Panda <56599943+Amitavpanda@users.noreply.github.com>
2023-05-22 20:22:51 +0530

    feat: style navbar links on hover and focus (#573)
    Co-authored-by: Priyankar Pal <88102392+priyankarpal@users.noreply.github.com>

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,8 @@ const Navbar = () => {
           <ul className="flex items-center gap-5 text-[1rem]">
             <li>
               <NavLink
-                to="/"
+                to="/" className={`inline-block py-2 px-3 text-center font-bold  rounded-md ${theme.mode === "light" ? "hover:text-white hover:bg-black focus:text-white focus:bg-black": "hover:text-black hover:bg-white focus:text-black focus:bg-white"}`}
+
                 className={`inline-block py-2 px-3 text-center font-bold rounded-md focus:outline-none focus:ring ${
                   theme.name === "light" ? "hover:text-white hover:bg-black" : "hover:text-black hover:bg-white"
                 }`}
@@ -52,13 +53,16 @@ const Navbar = () => {
                   fontWeight: "bold",
                   color: navbar.color,
                   backgroundColor: "white",
-                }}
-              >
+                }}>
                 Home
               </NavLink>
             </li>
             <li>
               <NavLink
+
+                to="/ProjectsPage"
+                className={`inline-block py-2 px-3 text-center font-bold  rounded-md ${theme.mode === "light" ? "hover:text-white hover:bg-black focus:text-white focus:bg-black": "hover:text-black hover:bg-white focus:text-black focus:bg-white"}`}
+
                 to="/projectspage"
                 className={`inline-block py-2 px-3 text-center font-bold rounded-md focus:outline-none focus:ring ${
                   theme.name === "light" ? "hover:text-white hover:bg-black" : "hover:text-black hover:bg-white"
@@ -74,6 +78,10 @@ const Navbar = () => {
             </li>
             <li>
               <NavLink
+
+                to="/AddYourProjectsGuide"
+                className={`inline-block py-2 px-3 text-center font-bold  rounded-md ${theme.mode === "light" ? "hover:text-white hover:bg-black focus:text-white focus:bg-black": "hover:text-black hover:bg-white focus:text-black focus:bg-white"}`}
+
                 to="/docs"
                 className={`inline-block py-2 px-3 text-center font-bold rounded-md focus:outline-none focus:ring ${
                   theme.name === "light" ? "hover:text-white hover:bg-black" : "hover:text-black hover:bg-white"
@@ -83,10 +91,21 @@ const Navbar = () => {
                   color: navbar.color,
                   backgroundColor: "white",
                 }}
+
               >
                 Docs
               </NavLink>
             </li>
+
+            <li>
+              <NavLink
+                to="/ContributorsPage"
+                className={`inline-block py-2 px-3 text-center font-bold  rounded-md ${theme.mode === "light" ? "hover:text-white hover:bg-black focus:text-white focus:bg-black": "hover:text-black hover:bg-white focus:text-black focus:bg-white"}`}
+              >
+                Contributors
+              </NavLink>
+            </li>
+
           </ul>
         </div>
 
