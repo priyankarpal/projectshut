4ab66de77c48e518357f18a39679d3ae027045ae
gkprasanth <gkpc2004@gmail.com>
2023-05-22 23:13:49 +0530

    Issue-223
    Aligned drop down menu to right side.

--- src/components/LanguageDropDown.jsx
+++ src/components/LanguageDropDown.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const LanguageDropDown = ({ onChange }) => {
   return (
-    <select className="p-4 bg-blue-700 text-white absolute top-0" onChange={onChange}>
+    <select className="p-2 bg-gray-400 text-black absolute top-0 right-0 px-5" onChange={onChange}>
       <option value="en">English</option>
       <option value="fr">French</option>
       <option value="ger">German</option>
