cf71cf71b451c18d7309f8f249a3cdf415228579
kai <kailashjat604@gmail.com>
2023-05-23 20:03:33 +0530

    Refactor to change the Router

---
+++ src/components/Layout.jsx
@@ -0,0 +1,29 @@
+import { useContext } from "react"
+import { Outlet } from "react-router-dom"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import { ThemeContext, ThemeProvider } from "../context/Theme"
+import ScrollToTop from "./ScrollToTop"
+
+export default function Layout() {
+  const { theme } = useContext(ThemeContext)
+
+  return (
+    <ThemeProvider>
+      <div
+        className="text-white font-lato"
+        style={{
+          background: theme?.background,
+          color: theme?.color,
+          buttonBgColor: theme?.button?.buttonBgColor,
+          buttonColor: theme?.button?.buttonColor,
+        }}
+      >
+        <Navbar />
+        <ScrollToTop />
+        <Outlet />
+        <Footer />
+      </div>
+    </ThemeProvider>
+  )
+}
