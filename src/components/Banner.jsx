b5867e5ef1154359a4deb8806cb270a8dad3d315
Priyankar Pal <88102392+priyankarpal@users.noreply.github.com>
2023-05-24 18:31:24 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -38,7 +38,7 @@ function Banner() {
                   Fork repository
                 </a>
                 <Link
-                  to="/AddYourProjectsGuide"
+                  to="/docs"
                   className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                 >
                   Add your projects <span aria-hidden="true">→</span>
