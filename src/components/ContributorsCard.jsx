c05a1a8c38e8b1b42904de697dcd6161cd9f432f
jibola2588 <jibolafawole258@gmail.com>
2023-05-24 12:13:16 +0100

    removed Bearer from authorization and added async/await with the optional chaining

--- src/components/ContributorsCard.jsx
+++ src/components/ContributorsCard.jsx
@@ -4,45 +4,40 @@ import process from "process"
 function Contributors({ owner, repo }) {
   const [contributors, setContributors] = useState([])
   const [loading, setLoading] = useState(true)
-  const [error,setError] = useState('')
+  const [error, setError] = useState("")
 
   useEffect(() => {
     setLoading(true) // set loading to true when th e fetch request is initiated
     async function fetchData() {
       try {
-        const response = await  fetch(`https://api.github.com/repos/${owner}/${repo}/contributors?per_page=100`, {
-             headers: {
-               Authorization: `${process.env.GITHUB_TOKEN}`,
-             },
-           })
-        const data = await response.json();
-        setContributors(data);
-        setLoading(false); // set loading to false when the fetch request is completed
+        const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/contributors?per_page=100`, {
+          headers: {
+            Authorization: `${process.env.GITHUB_TOKEN}`,
+          },
+        })
+        const data = await response.json()
+        setContributors(data)
+        setLoading(false) // set loading to false when the fetch request is completed
       } catch (error) {
-        setError(error.message);
-        setLoading(false); // set loading to false when an error occurs
+        setError(error.message)
+        setLoading(false) // set loading to false when an error occurs
       }
     }
-    
-    fetchData();
+
+    fetchData()
   }, [owner, repo])
 
   return (
     <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-8 justify-items-center items-center ">
-      {loading ?
-       (
+      {loading ? (
         <div className="flex justify-center items-center h-full col-span-10">
           <span className="text-primary text-xl font-semibold">Loading...</span>
         </div>
-      ) 
-      : error ? 
-       (
+      ) : error ? (
         <div className="flex justify-center items-center h-full col-span-10">
-          <p className="text-red-600 text-lg">{error}</p> 
-      </div>
-      ) 
-      :
-       (
+          <p className="text-red-600 text-lg">{error}</p>
+        </div>
+      ) : (
         contributors?.map((contributor) => (
           <div className="min-w-max p-2" key={contributor.id}>
             <a href={contributor.html_url} target="_blank">
