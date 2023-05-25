c29c3c3922df03bf269d8b51ee5fd2cf2cc3cfad
Jeyavishnu S <sjeyavishnu9865@gmail.com>
2023-05-25 14:53:09 +0530

    fix:Contributor:Further Isolate ContributorCard

--- src/components/ContributorCard.jsx
+++ src/components/ContributorCard.jsx
@@ -1,15 +1,13 @@
 import React from "react"
 
-function ContributorCard({ contributors }) {
+function ContributorCard({ id, login, html_url, avatar_url }) {
   return (
     <>
-      {contributors?.map((contributor) => (
-        <div className="min-w-max p-2" key={contributor.id}>
-          <a href={contributor.html_url} target="_blank">
-            <img className="h-16 mb-3 rounded-full " src={contributor.avatar_url} alt={contributor.login} />
-          </a>
-        </div>
-      ))}
+      <div className="min-w-max p-2" key={id}>
+        <a href={html_url} target="_blank">
+          <img className="h-16 mb-3 rounded-full " src={avatar_url} alt={login} />
+        </a>
+      </div>
     </>
   )
 }
--- src/pages/ContributorPage.jsx
+++ src/pages/ContributorPage.jsx
@@ -3,15 +3,12 @@ import process from "process"
 import ContributorCard from "@/components/ContributorCard"
 
 function ContriButorsPage() {
+  // hooks for contributor section
   const [contributors, setContributors] = useState([])
   const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    getContributorData()
-  }, [])
-
   const getContributorData = async () => {
-    setLoading(true) // set loading to true when th e fetch request is initiated
+    setLoading(true) // set loading to true when the fetch request is initiated
     await fetch(`https://api.github.com/repos/priyankarpal/ProjectsHut/contributors?per_page=100`, {
       headers: {
         Authorization: ` ${process.env.GITHUB_TOKEN}`,
@@ -28,18 +25,31 @@ function ContriButorsPage() {
       })
   }
 
+  useEffect(() => {
+    getContributorData()
+  }, [])
+
   return (
     <main className=" my-8 min-h-[65.75vh] max-w-6xl w-11/12 mx-auto sm:min-h-[73vh] lg:min-h-[78vh]">
       <h1 className=" text-[1.5rem] md:text-[3rem] pb-5 font-bold  text-center">
         Our <span className="text-primary">Contributors </span>
       </h1>
       {loading ? (
+        // display this while loading
         <div className="flex justify-center items-center h-full col-span-10">
           <span className="text-primary text-xl font-semibold">Loading...</span>
         </div>
       ) : (
+        // display contributors after loading
         <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-8 justify-items-center items-center ">
-          <ContributorCard contributors={contributors} />
+          {contributors?.map((contributor) => (
+            <ContributorCard
+              id={contributor.id}
+              login={contributor.login}
+              html_url={contributor.html_url}
+              avatar_url={contributor.avatar_url}
+            />
+          ))}
         </section>
       )}
     </main>
