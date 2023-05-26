2d999e215adcf3b43a678b718c849b1e4e57a1c5
Manik Dingra <manikdhingra0582@gmail.com>
2023-05-26 23:44:43 +0530

    Lazy loading enabled and loader component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,51 @@
-import { ProjectsPage, HomePage, AddYourProjectsGuide } from './pages';
 import { Route, RouterProvider, createRoutesFromElements, createBrowserRouter } from 'react-router-dom';
 import PageNotFound from './components/PageNotFound';
-import ProjectList from './components/ProjectList';
 import Layout from './components/Layout';
 import { ThemeProvider } from './context/Theme';
+import { lazy, Suspense } from "react"
+import Loader from "./utils/Loader"
+
+const ProjectsPage = lazy(() => import("./pages/ProjectsPage"));
+const HomePage = lazy(() => import("./pages/HomePage"));
+const AddYourProjectsGuide = lazy(() => import("./pages/AddYourProjectsGuide"));
+const ProjectList = lazy(() => import("./components/ProjectList"));
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route index element={<HomePage />} />
-      <Route path="projectspage" element={<ProjectsPage />} />
-      <Route path="projects/:username" element={<ProjectList />} />
-      <Route path="docs" element={<AddYourProjectsGuide />} />
+      <Route index element={
+        <Suspense fallback={<Loader />}>
+          <HomePage />
+        </Suspense>
+      } />
+      <Route path="/projectspage" element={
+        <Suspense fallback={<Loader />}>
+          <ProjectsPage />
+        </Suspense>
+      } />
+      <Route path="projects/:username" element={
+        <Suspense fallback={<Loader />}>
+          <ProjectList />
+        </Suspense>
+      } />
+      <Route path="/docs" element={
+        <Suspense fallback={<Loader />}>
+          <AddYourProjectsGuide />
+        </Suspense>
+      } />
       <Route path="*" element={<PageNotFound />} />
     </Route>,
   ),
 );
+
 
 function App() {
   return (
     <ThemeProvider>
       <RouterProvider router={router} />
     </ThemeProvider>
-  );
+  )
 }
 
-export default App;
+export default App
+
