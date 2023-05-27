883a5273d8cac00972dae4b51119b702bc323d1c
Suzy <sulagnaghosh52@gmail.com>
2023-05-27 22:11:01 +0530

    feat: add search project option

---
+++ src/utils/searchProject.js
@@ -0,0 +1,15 @@
+export const searchProject = (projects, name) => {
+  let resultArr = []
+  if (name.length > 0) {
+    for (const project of projects) {
+      for (const item of project.Projects) {
+        if (item.title === name) {
+          resultArr.push(project)
+        }
+      }
+    }
+
+  }
+
+  return resultArr
+}
