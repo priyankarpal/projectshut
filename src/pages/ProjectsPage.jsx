6461f57f61f172d59a703e8abccc6d0e4e46af5d
kai <kailashjat604@gmail.com>
2023-05-31 10:46:02 +0530

    refactor: retain filter

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -189,6 +189,7 @@ const ProjectsPage = () => {
               listOfProjects={item['Projects']}
               socaialMedia={item['Social_media']}
               key={i}
+              filter={currentFilter}
             />
           ))
         ) : (
