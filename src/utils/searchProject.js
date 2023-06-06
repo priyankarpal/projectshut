131254e4403587f9be76a74be6b30893755a2bac
Suzy <sulagnaghosh52@gmail.com>
2023-06-06 23:34:10 +0530

    chore: pnpm and eslint fix

--- src/utils/searchProject.js
+++ src/utils/searchProject.js
@@ -1,11 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable linebreak-style */
+/* eslint-disable import/prefer-default-export */
 export const searchProject = (projects, name) => {
-  let resultArr = [];
+  const resultArr = [];
   if (name.length > 0) {
     for (const project of projects) {
       for (const item of project.Projects) {
         if (item.title.toLowerCase().includes(name.toLowerCase())) {
           resultArr.push(project);
-          break;
         }
       }
     }
