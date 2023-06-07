888824d7390ecb327ba154558d2a47dabb35fada
Suzy <sulagnaghosh52@gmail.com>
2023-06-07 23:05:29 +0530

    fix: solve search error

--- src/utils/searchProject.js
+++ src/utils/searchProject.js
@@ -1,10 +1,13 @@
+/* eslint-disable padded-blocks */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable prefer-const */
+/* eslint-disable import/prefer-default-export */
 export const searchProject = (projects, title) => {
   let resultArr = [];
   if (title.length > 0) {
     for (const project of projects) {
       if (project.title.toLowerCase().includes(title.toLowerCase())) {
         resultArr.push(project);
-        break;
       }
     }
   }
