c6f5717f5d88680ddf66357ad26d53ea583aa078
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-06-23 14:04:03 +0530

    banner button removed

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -41,13 +41,6 @@ function Banner() {
             >
               Fork repository
             </a>
-            <Link
-              href="/docs"
-              className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white transition duration-300 ease-in-out cursor-pointer focus:outline-none hover:shadow-lg transform hover:-translate-y-1 active:translate-y-0"
-              type="button"
-            >
-              Add your projects <span aria-hidden="true">→</span>
-            </Link>
           </div>
         </div>
         <div className="relative mt-16 lg:mt-8 dev-img-parent-class ">
