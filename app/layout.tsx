b436422ea61ada04a008f882adfefd14210c4324
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-07-06 20:32:09 +0530

    chore: remove exgra divs

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} suppressHydrationWarning={true}>
+        {children}
+      </body>
     </html>
   );
 }
