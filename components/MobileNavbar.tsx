2197b80c1de51ceaad3a4ced9b927e02cb2daa29
MeenuyD <meenu.coninja@gmail.com>
2023-07-12 23:29:14 +0530

    Hover effect is added for mobaile device of nav links

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -10,17 +10,17 @@ export const MobileNavBar = () => {
         <footer className="fixed bottom-1 w-1/11  left-0 right-0 bg-gray-800 text-white py-4 md:hidden rounded-xl mx-5">
             <div className="container mx-auto text-xl flex justify-between items-center px-4">
                 <Link href="/">
-                    <AiFillHome />
+                    <AiFillHome className="text-primary hover:text-primary-dark"/>
                 </Link>
                 <Link href='/projects'>
-                    <MdExplore />
+                    <MdExplore className="text-primary hover:text-primary-dark" />
                 </Link>
                 <Link href='/docs'>
 
-                    <BiSolidBookBookmark />
+                    <BiSolidBookBookmark className="text-primary hover:text-primary-dark" />
                 </Link>
                 <Link href='https://github.com/priyankarpal/ProjectsHut'>
-                    <FaGithub />
+                    <FaGithub className="text-primary hover:text-primary-dark" />
                 </Link>
             </div>
         </footer>
