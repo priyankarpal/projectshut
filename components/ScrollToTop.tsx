2ef0414f643abfa97c7f5d8e028a21f49fb56e26
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-07-17 12:46:25 +0530

    made suggested changes

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -1,15 +1,16 @@
+"use client"
 import React, { useEffect, useState } from 'react';
 import { FaArrowUp } from 'react-icons/fa';
-import { Link } from 'react-scroll';
 
 function BackToTopButton() {
   const [btnVisiblity, setBtnVisiblity] = useState(false);
+  
   const handleScroll = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
   useEffect(() => {
     const toggleVisiblity = () => {
-      window.pageYOffset > 250 ? setBtnVisiblity(true) : setBtnVisiblity(false);
+      window.scrollY > 250 ? setBtnVisiblity(true) : setBtnVisiblity(false);
     };
     window.addEventListener('scroll', toggleVisiblity);
     return () => {
@@ -18,7 +19,7 @@ function BackToTopButton() {
   }, []);
 
   return (
-    <Link onClick={handleScroll} to="/" className="top">
+    <button onClick={handleScroll}>
       <div
         className={` ${!btnVisiblity ? 'translate-y-32 md:translate-y-20' : 'translate-y-0'
           } fixed bottom-20 md:bottom-10 right-8 z-50 w-10 h-10 rounded-lg bg-primary text-white cursor-pointer flex justify-center items-center transform transition-all delay-100 duration-500 `}
@@ -27,7 +28,7 @@ function BackToTopButton() {
           <FaArrowUp />
         </div>
       </div>
-    </Link>
+    </button>
   );
 };
 
