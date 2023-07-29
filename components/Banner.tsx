c4822062b6a501c292fbf608b9a04bb9b6e29adb
SouravvMishra <souravvmishra@gmail.com>
2023-07-29 16:33:16 +0530

    Prevent Text Selection

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -27,7 +27,7 @@ const features = [
 
 export default function Banner() {
   return (
-    <div className="bg-inherit py-24 sm:py-32">
+    <div className="bg-inherit py-24 sm:py-32 select-none">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-primary">
