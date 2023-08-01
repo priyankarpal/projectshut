86585d1b89e24bd572d209c0b9067463854b5ec2
Tushar Saini <s2sharpit@gmail.com>
2023-08-01 22:54:07 +0530

    feat: implementing dynamic title to Projects dynamic route
    Signed-off-by: Tushar Saini <s2sharpit@gmail.com>

--- app/projects/[username]/page.tsx
+++ app/projects/[username]/page.tsx
@@ -1,7 +1,15 @@
 import ProjectList from "@/components/projects/ProjectList";
+import { Metadata, ResolvingMetadata } from 'next'
 
-export const metadata = {
-  title: 'Projects',
+type Props = {
+  params: { username: string }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const title = params.username[0].toUpperCase() + params.username.slice(1)
+  return {
+    title: title,
+  }
 }
 
 export default function page() {
