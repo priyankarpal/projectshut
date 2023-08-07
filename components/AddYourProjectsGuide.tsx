ec7d6665196db6aa46b4f1c2ab8ec3779c92d817
Parul Pandey <parulss1606@gmail.com>
2023-08-07 18:31:50 +0530

    docs: improve site docs (#1987)
    * docs: improve site docs
    
    * docs: improve site docs
    
    * Update AddYourProjectsGuide.tsx
    
    Signed-off-by: Parul Pandey <parulss1606@gmail.com>
    
    * docs: improve site docs
    
    * docs: improve sites's docs
    
    ---------
    
    Signed-off-by: Parul Pandey <parulss1606@gmail.com>
    Co-authored-by: Priyankar Pal <88102392+priyankarpal@users.noreply.github.com>

--- components/AddYourProjectsGuide.tsx
+++ components/AddYourProjectsGuide.tsx
@@ -48,14 +48,19 @@ const option1Steps = [
     text: "Fork this repository",
     code: "",
     image:
-      "https://user-images.githubusercontent.com/88102392/226444075-7d7d28b5-8d88-459a-bb82-38a3f64aaf28.png",
+      "https://user-images.githubusercontent.com/80768852/258651696-63e1bf34-d2b6-4963-b3df-6accc3d56941.jpg",
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
-    text: "Click on Go to file (I'm doing from priyankarpal/projectshut)",
-    code: "",
+    text: "Create a new Branch(click on main -> add name of your branch -> click on create branch:)",
+    image: 
+      "https://user-images.githubusercontent.com/80768852/258656607-64ad9db4-11a5-4dd8-ade8-21875c8831f7.jpg"
+  },
+  {
+    icon: <PiArrowFatLineRightFill size={18} color="white" />,
+    text: "Click on Go to file",
     image:
-      "https://user-images.githubusercontent.com/88102392/226444608-12a2abb9-436c-4843-8893-49029cb4c033.png",
+      "https://user-images.githubusercontent.com/80768852/258656827-b70c96bf-b8b9-4d0e-872e-c1f973841f25.jpg"
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
@@ -65,22 +70,30 @@ const option1Steps = [
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
     text: "Commit your changes",
-    code: "",
     image:
-      "https://user-images.githubusercontent.com/88102392/232192543-5fcc2711-7693-4dba-8993-75e3528b8530.png",
+      "https://user-images.githubusercontent.com/80768852/258656924-387f55b3-cd7c-4d6e-a25d-4043330e8d2b.png",
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
-    text: "Fill up the description",
-    code: "",
+    text: "To create a pull request click on compare and pull request",
+    image:
+      "https://user-images.githubusercontent.com/80768852/258657644-b1bc753b-3c0a-4853-8578-dde040c2665e.jpg"
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
-    text: "Create Pull Request & wait for merge",
+    text: "Add an appropriate title and description to your PR explaining changes"
+  },
+  {
+    icon: <PiArrowFatLineRightFill size={18} color="white" />,
+    text: "Click on Create Pull Request & wait for merge",
     code: "",
     image:
-      "https://user-images.githubusercontent.com/88102392/232193469-dcc2b0b8-ec6b-4023-a9f4-bb6900ae8aff.png",
+      "https://user-images.githubusercontent.com/80768852/258657487-9fa9558c-e49b-4647-a9ee-50da86b9fccf.jpg",
   },
+  {
+    icon: <PiArrowFatLineRightFill size={18} color="white" />,
+    text: "Congratulations🎉, you have made a PR to the ProjectsHut. Wait for your submission to be accepted and your PR to be merged by a maintainer."
+  }
 ];
 
 const option2Steps = [
@@ -91,58 +104,118 @@ const option2Steps = [
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
-    text: "Clone this repository",
-    code: "git clone https://github.com/[your-username]/ProjectsHut.git",
+    text: "Fork this repository",
+    code: "",
+    image:
+      "https://user-images.githubusercontent.com/80768852/258651696-63e1bf34-d2b6-4963-b3df-6accc3d56941.jpg",
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
-    text: "Navigate to the project folder",
+    text: "Clone the repository",
+    code: "git clone https://github.com/<your_username>/ProjectsHut.git",
+    image: 
+      "https://user-images.githubusercontent.com/80768852/258655867-6c74b8f4-ee2f-48e8-8fc1-38679143175f.png"
+  },
+  {
+    icon: <PiArrowFatLineRightFill size={18} color="white" />,
+    text: "Navigate to the ProjectsHut directory",
     code: "cd ProjectsHut",
+    image: 
+      "https://user-images.githubusercontent.com/80768852/258653517-fefecfe1-1f74-444b-8837-120c9f57cebf.png"
+  },
+  {
+    icon: <PiArrowFatLineRightFill size={18} color="white" />,
+    text: "Add a reference(remote) to the original repository",
+    code: "git remote add upstream https://github.com/priyankarpal/ProjectsHut.git",
+    image:
+      "https://user-images.githubusercontent.com/80768852/258653648-497133fd-c756-4065-9956-d2457b4ff53c.png"
+  },
+  {
+    icon: <PiArrowFatLineRightFill size={18} color="white" />,
+    text: "Check remotes for this repository",
+    code: "git remote -v",
+    image:
+      "https://user-images.githubusercontent.com/80768852/258653764-685d1dea-627c-4e4c-9444-c1ff05fb444e.png" 
+  },
+  {
+    icon: <PiArrowFatLineRightFill size={18} color="white" />,
+    text: "Always take a pull from the upstream repository to your main branch to keep it updated as per the main project repository",
+    code: "git pull upstream main",
+    image:
+      "https://user-images.githubusercontent.com/80768852/258656123-fea37c96-d16b-43e3-807d-76a2c01a9d4a.png"
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
     text: "install dependencies",
     code: "pnpm i",
+    image:
+      "https://user-images.githubusercontent.com/80768852/258655966-ea8b5390-32d8-4f9e-8eb2-1eb840e9e7d1.png"
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
-    text: "Create a new branch using your `GitHub Username`",
-    code: "git checkout -b [name_of_your_new_branch]",
+    text: "Create a new Branch",
+    code: "git checkout -b <your_branch_name>",
+    image: 
+      "https://user-images.githubusercontent.com/80768852/258654058-e0e8a964-56d1-4c33-91aa-0334771e8034.png"
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
     text: "Run the project",
     code: "pnpm dev",
+    image:
+      "https://user-images.githubusercontent.com/80768852/258656216-24308a09-ae5a-4e7c-9c08-fbdd48e7f017.png"
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
-    text: "Do Changes in Project you want.",
-    code: "",
+    text: "Perform your desired Changes to the code base",
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
-    text: "Add your changes",
-    code: "git add .",
+    text: "Check your changes",
+    code: "git status",
+    image:
+      "https://user-images.githubusercontent.com/80768852/258656155-874b051c-165a-4bde-8d2d-a32b05c0ac54.png"
+  },
+  {
+    icon: <PiArrowFatLineRightFill size={18} color="white" />,
+    text: "Stage your changes",
+    code: "git add . </files_that_you_made_changes>",
+    image:
+      "https://user-images.githubusercontent.com/80768852/258654878-57d38c53-f5f4-476f-b5a9-c500dcc3403b.png"
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
     text: "Commit your changes",
-    code: 'git commit -m "Added [your-changes]"',
+    code: "git commit -m relevant message (add relevant message according to your issue and always add double quote to your message)",
+    image:
+      "https://user-images.githubusercontent.com/80768852/258654990-9a2685db-9419-4bb8-9899-821104a437a5.png",
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
-    text: "Set upstream command",
-    code: "git remote add upstream https://github.com/priyankarpal/ProjectsHut.git",
+    text: "Push your changes to the main branch",
+    code: "git push -u origin <your_branch_name>",
+    image:
+      "https://user-images.githubusercontent.com/80768852/258654547-7a0e300b-aada-4fa3-b1de-90d445c0311e.png"
+  },{
+    icon: <PiArrowFatLineRightFill size={18} color="white" />,
+    text: "To create a pull request click on compare and pull request",
+    image:
+      "https://user-images.githubusercontent.com/80768852/258657644-b1bc753b-3c0a-4853-8578-dde040c2665e.jpg"
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
-    text: "Push your changes",
-    code: "git push origin [name_of_your_branch]",
+    text: "Add an appropriate title and description to your PR explaining changes"
   },
   {
     icon: <PiArrowFatLineRightFill size={18} color="white" />,
-    text: "Create Pull Request & wait for merge",
+    text: "Click on Create Pull Request & wait for merge",
     code: "",
+    image:
+      "https://user-images.githubusercontent.com/80768852/258657487-9fa9558c-e49b-4647-a9ee-50da86b9fccf.jpg",
+  },
+  {
+    icon: <PiArrowFatLineRightFill size={18} color="white" />,
+    text: "Congratulations🎉, you have made a PR to the ProjectsHut. Wait for your submission to be accepted and your PR to be merged by a maintainer."
   },
 ];
 
