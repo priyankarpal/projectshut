237c0f57b0c286e66d7c22c50028f3bcc782fb7b
Subham Bhattacharjee <94003231+SubhamB2003@users.noreply.github.com>
2023-08-10 15:05:13 +0530

    feat: add pwa to projectshut (#2044)
    * Added PWA
    
    * Added PWA
    
    * fix: changes done
    
    * fix: changes done
    
    * fix: changes done
    
    * fix: changes done
    
    * fix: changes done
    
    ---------
    
    Co-authored-by: Priyankar Pal <88102392+priyankarpal@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,43 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
+import { Footer, Navbar, ScrollToTop } from "@/components";
 import { MobileNavBar } from "@/components/MobileNavbar";
-import { Navbar, Footer, ScrollToTop } from "@/components";
+import "@/public/manifest.json";
 import { Metadata } from "next";
-
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://projectshut.vercel.app/"),
+  manifest: "/manifest.json",
   title: {
     default: "ProjectsHut - Learn. Build. Share.",
-    template: '%s - ProjectsHut',
+    template: "%s - ProjectsHut",
   },
-  description: "A platform where you can share your open source projects with the world.",
-  keywords: ["ProjectHut", "projects", "educational resources","wide range of products", "high-quality projects", "React", "Nodejs", "JavaScript", "open source", "contribution", "learners"],
-  authors: {name: "Priyankar Pal"},
+  description:
+    "A platform where you can share your open source projects with the world.",
+  keywords: [
+    "ProjectHut",
+    "projects",
+    "educational resources",
+    "wide range of products",
+    "high-quality projects",
+    "React",
+    "Nodejs",
+    "JavaScript",
+    "open source",
+    "contribution",
+    "learners",
+  ],
+  authors: { name: "Priyankar Pal" },
   openGraph: {
-    title: 'ProjectsHut - Learn. Build. Share.',
-    description: 'A platform where you can share your open source projects with the world.',
-    url: 'https://projectshut.vercel.app',
-    siteName: 'ProjectHut',
-    locale: 'en-US',
-    type: 'website',
+    title: "ProjectsHut - Learn. Build. Share.",
+    description:
+      "A platform where you can share your open source projects with the world.",
+    url: "https://projectshut.vercel.app",
+    siteName: "ProjectHut",
+    locale: "en-US",
+    type: "website",
   },
   robots: {
     index: true,
@@ -36,10 +51,12 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: 'ProjectsHut - Learn. Build. Share.',
-    card: 'summary_large_image',
-    description: "A platform where you can share your open source projects with the world.",
-    images: "https://user-images.githubusercontent.com/88102392/251507361-e71396f9-92c6-4664-b7e4-4275cb902e65.png"
+    title: "ProjectsHut - Learn. Build. Share.",
+    card: "summary_large_image",
+    description:
+      "A platform where you can share your open source projects with the world.",
+    images:
+      "https://user-images.githubusercontent.com/88102392/251507361-e71396f9-92c6-4664-b7e4-4275cb902e65.png",
   },
 };
 
