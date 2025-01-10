425e3ba9e46f3293cccd44191ad9d8739cba7b92
kyday <kyday63@gmail.com>
2025-01-10 22:24:34 +0900

    fix: improve loading Skeleton styles

--- components/projects/Loading.tsx
+++ components/projects/Loading.tsx
@@ -41,10 +41,10 @@ export const Loading = {
     <div className="h-full shadow rounded-md p-4 max-w-sm w-full mx-auto">
       <div className="animate-pulse flex flex-col justify-center items-start">
         <div className="flex justify-between items-center w-full">
-          <div className="w-60  bg-slate-700 rounded  h-2 " />
-          <div className="rounded-full bg-slate-700 h-10 w-10" />
+          <div className="w-60  bg-slate-700 rounded h-2 mr-8" />
+          <div className="rounded-full bg-slate-700 h-10 w-10 flex-shrink-0" />
         </div>
-        <div className=" py-1 w-full">
+        <div className="py-1 w-full">
           <div className="grid">
             <div className="h-2 my-1  bg-slate-700 rounded col-span-2" />
             <div className="h-2  bg-slate-700 rounded col-span-1" />
