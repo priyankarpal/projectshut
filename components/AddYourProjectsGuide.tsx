4ae8d7fed47b527939e7addb1de239c9a73d60d1
SnowyCrest <anas.timeridjine@gmail.com>
2025-02-14 15:14:39 +0300

    fix(docs): grammatical fix and made text responsive to the theme

--- components/AddYourProjectsGuide.tsx
+++ components/AddYourProjectsGuide.tsx
@@ -7,7 +7,6 @@ import { MdOutlineContentCopy } from "react-icons/md";
 import { BsCheck2Circle } from "react-icons/bs";
 import { Motion } from "@/components/framer-motion";
 
-
 type StepProps = {
   steps: string;
   text: string;
@@ -45,14 +44,14 @@ const option1Steps = [
   {
     steps: "Step 2 : ",
     text: "Create a new Branch(click on main -> add name of your branch -> click on create branch:)",
-    image: 
-      "https://user-images.githubusercontent.com/80768852/258656607-64ad9db4-11a5-4dd8-ade8-21875c8831f7.jpg"
+    image:
+      "https://user-images.githubusercontent.com/80768852/258656607-64ad9db4-11a5-4dd8-ade8-21875c8831f7.jpg",
   },
   {
     steps: "Step 3 : ",
     text: "Click on Go to file",
     image:
-      "https://user-images.githubusercontent.com/80768852/258656827-b70c96bf-b8b9-4d0e-872e-c1f973841f25.jpg"
+      "https://user-images.githubusercontent.com/80768852/258656827-b70c96bf-b8b9-4d0e-872e-c1f973841f25.jpg",
   },
   {
     steps: "Step 4 : ",
@@ -69,11 +68,11 @@ const option1Steps = [
     steps: "Step 6 : ",
     text: "To create a pull request click on compare and pull request",
     image:
-      "https://user-images.githubusercontent.com/80768852/258657644-b1bc753b-3c0a-4853-8578-dde040c2665e.jpg"
+      "https://user-images.githubusercontent.com/80768852/258657644-b1bc753b-3c0a-4853-8578-dde040c2665e.jpg",
   },
   {
     steps: "Step 7 : ",
-    text: "Add an appropriate title and description to your PR explaining changes"
+    text: "Add an appropriate title and description to your PR explaining changes",
   },
   {
     steps: "Step 8 : ",
@@ -83,8 +82,8 @@ const option1Steps = [
       "https://user-images.githubusercontent.com/80768852/258657487-9fa9558c-e49b-4647-a9ee-50da86b9fccf.jpg",
   },
   {
-    text: "Congratulations🎉, you have made a PR to the ProjectsHut. Wait for your submission to be accepted and your PR to be merged by a maintainer."
-  }
+    text: "Congratulations🎉, you have made a PR to the ProjectsHut. Wait for your submission to be accepted and your PR to be merged by a maintainer.",
+  },
 ];
 
 const option2Steps = [
@@ -104,57 +103,57 @@ const option2Steps = [
     steps: "Step 2 : ",
     text: "Clone the repository",
     code: "git clone https://github.com/<your_username>/ProjectsHut.git",
-    image: 
-      "https://user-images.githubusercontent.com/80768852/258655867-6c74b8f4-ee2f-48e8-8fc1-38679143175f.png"
+    image:
+      "https://user-images.githubusercontent.com/80768852/258655867-6c74b8f4-ee2f-48e8-8fc1-38679143175f.png",
   },
   {
     steps: "Step 3 : ",
     text: "Navigate to the ProjectsHut directory",
     code: "cd ProjectsHut",
-    image: 
-      "https://user-images.githubusercontent.com/80768852/258653517-fefecfe1-1f74-444b-8837-120c9f57cebf.png"
+    image:
+      "https://user-images.githubusercontent.com/80768852/258653517-fefecfe1-1f74-444b-8837-120c9f57cebf.png",
   },
   {
     steps: "Step 4 : ",
     text: "Add a reference(remote) to the original repository",
     code: "git remote add upstream https://github.com/priyankarpal/ProjectsHut.git",
     image:
-      "https://user-images.githubusercontent.com/80768852/258653648-497133fd-c756-4065-9956-d2457b4ff53c.png"
+      "https://user-images.githubusercontent.com/80768852/258653648-497133fd-c756-4065-9956-d2457b4ff53c.png",
   },
   {
     steps: "Step 5 : ",
     text: "Check remotes for this repository",
     code: "git remote -v",
     image:
-      "https://user-images.githubusercontent.com/80768852/258653764-685d1dea-627c-4e4c-9444-c1ff05fb444e.png" 
+      "https://user-images.githubusercontent.com/80768852/258653764-685d1dea-627c-4e4c-9444-c1ff05fb444e.png",
   },
   {
     steps: "Step 6 : ",
     text: "Always take a pull from the upstream repository to your main branch to keep it updated as per the main project repository",
     code: "git pull upstream main",
     image:
-      "https://user-images.githubusercontent.com/80768852/258656123-fea37c96-d16b-43e3-807d-76a2c01a9d4a.png"
+      "https://user-images.githubusercontent.com/80768852/258656123-fea37c96-d16b-43e3-807d-76a2c01a9d4a.png",
   },
   {
     steps: "Step 7 : ",
     text: "install dependencies",
     code: "pnpm i",
     image:
-      "https://user-images.githubusercontent.com/80768852/258655966-ea8b5390-32d8-4f9e-8eb2-1eb840e9e7d1.png"
+      "https://user-images.githubusercontent.com/80768852/258655966-ea8b5390-32d8-4f9e-8eb2-1eb840e9e7d1.png",
   },
   {
     steps: "Step 8 : ",
     text: "Create a new Branch",
     code: "git checkout -b <your_branch_name>",
-    image: 
-      "https://user-images.githubusercontent.com/80768852/258654058-e0e8a964-56d1-4c33-91aa-0334771e8034.png"
+    image:
+      "https://user-images.githubusercontent.com/80768852/258654058-e0e8a964-56d1-4c33-91aa-0334771e8034.png",
   },
   {
     steps: "Step 9 : ",
     text: "Run the project",
     code: "pnpm dev",
     image:
-      "https://user-images.githubusercontent.com/80768852/258656216-24308a09-ae5a-4e7c-9c08-fbdd48e7f017.png"
+      "https://user-images.githubusercontent.com/80768852/258656216-24308a09-ae5a-4e7c-9c08-fbdd48e7f017.png",
   },
   {
     steps: "Step 10 : ",
@@ -165,19 +164,19 @@ const option2Steps = [
     text: "Check your changes",
     code: "git status",
     image:
-      "https://user-images.githubusercontent.com/80768852/258656155-874b051c-165a-4bde-8d2d-a32b05c0ac54.png"
+      "https://user-images.githubusercontent.com/80768852/258656155-874b051c-165a-4bde-8d2d-a32b05c0ac54.png",
   },
   {
     steps: "Step 12 : ",
     text: "Stage your changes",
     code: "git add . </files_that_you_made_changes>",
     image:
-      "https://user-images.githubusercontent.com/80768852/258654878-57d38c53-f5f4-476f-b5a9-c500dcc3403b.png"
+      "https://user-images.githubusercontent.com/80768852/258654878-57d38c53-f5f4-476f-b5a9-c500dcc3403b.png",
   },
   {
     steps: "Step 13 : ",
     text: "Commit your changes",
-    code: "git commit -m relevant message (add relevant message according to your issue and always add double quote to your message)",
+    code: 'git commit -m "relevant message" (add relevant message according to your issue and always add double quote to your message)',
     image:
       "https://user-images.githubusercontent.com/80768852/258654990-9a2685db-9419-4bb8-9899-821104a437a5.png",
   },
@@ -186,16 +185,17 @@ const option2Steps = [
     text: "Push your changes to the main branch",
     code: "git push -u origin <your_branch_name>",
     image:
-      "https://user-images.githubusercontent.com/80768852/258654547-7a0e300b-aada-4fa3-b1de-90d445c0311e.png"
-  },{
+      "https://user-images.githubusercontent.com/80768852/258654547-7a0e300b-aada-4fa3-b1de-90d445c0311e.png",
+  },
+  {
     steps: "Step 15 : ",
     text: "To create a pull request click on compare and pull request",
     image:
-      "https://user-images.githubusercontent.com/80768852/258657644-b1bc753b-3c0a-4853-8578-dde040c2665e.jpg"
+      "https://user-images.githubusercontent.com/80768852/258657644-b1bc753b-3c0a-4853-8578-dde040c2665e.jpg",
   },
   {
     steps: "Step 16 : ",
-    text: "Add an appropriate title and description to your PR explaining changes"
+    text: "Add an appropriate title and description to your PR explaining changes",
   },
   {
     steps: "Step 17 : ",
@@ -205,7 +205,7 @@ const option2Steps = [
       "https://user-images.githubusercontent.com/80768852/258657487-9fa9558c-e49b-4647-a9ee-50da86b9fccf.jpg",
   },
   {
-    text: "Congratulations🎉, you have made a PR to the ProjectsHut. Wait for your submission to be accepted and your PR to be merged by a maintainer."
+    text: "Congratulations🎉, you have made a PR to the ProjectsHut. Wait for your submission to be accepted and your PR to be merged by a maintainer.",
   },
 ];
 
@@ -222,23 +222,27 @@ function Step({ steps, text, code, image, index }: StepProps): JSX.Element {
   };
 
   return (
-    <Motion.div className="mt-5 flex flex-col gap-5 "
+    <Motion.div
+      className="mt-5 flex flex-col gap-5"
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0 }}
       viewport={{ once: true }}
-      transition={{ duration: 0.5, type: "spring", stiffness: 110 }}
-    >
-
-      <div className="flex items-center justify-between ">
+      transition={{ duration: 0.5, type: "spring", stiffness: 110 }}>
+      <div className="flex items-center justify-between">
         <Motion.div
           key={text}
           initial={{ opacity: 0, x: -50 }}
           whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true }}
-          transition={{ duration: 0.5, type: "spring", stiffness: 110, delay: index * 0.15 }}
-          className="flex items-center justify-between gap-5 w-full ">
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            stiffness: 110,
+            delay: index * 0.15,
+          }}
+          className="flex items-center justify-between gap-5 w-full">
           <div className="flex items-center gap-5">
-            <p className="text-base font-semibold leading-7 text-white">
+            <p className="text-base font-semibold leading-7 text-gray-900 dark:text-white">
               <b className="text-primary">{steps}</b>
               {text}
             </p>
@@ -248,14 +252,21 @@ function Step({ steps, text, code, image, index }: StepProps): JSX.Element {
               initial={{ opacity: 0, x: -50 }}
               whileInView={{ opacity: 1, x: 0 }}
               viewport={{ once: true }}
-              transition={{ duration: 0.5, type: "spring", stiffness: 110, delay: index * 0.20 }}
+              transition={{
+                duration: 0.5,
+                type: "spring",
+                stiffness: 110,
+                delay: index * 0.2,
+              }}
               className="flex items-end justify-end">
               <button
                 onClick={copyCode}
-                className="cursor-pointer text-indigo-600"
-              >
+                className="cursor-pointer text-gray-600 dark:text-indigo-600">
                 {copyCodeButton && (
-                  <MdOutlineContentCopy size="18" color="white" />
+                  <MdOutlineContentCopy
+                    size="18"
+                    className="text-gray-600 dark:text-white"
+                  />
                 )}
                 {!copyCodeButton && <BsCheck2Circle size="18" color="green" />}
               </button>
@@ -269,7 +280,12 @@ function Step({ steps, text, code, image, index }: StepProps): JSX.Element {
           initial={{ opacity: 0, x: -50 }}
           whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true }}
-          transition={{ duration: 0.5, type: "spring", stiffness: 110, delay: index * 0.25 }}
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            stiffness: 110,
+            delay: index * 0.25,
+          }}
           className="mt-2">
           <SyntaxHighlighter language="nginx" style={anOldHope}>
             {code}
@@ -281,8 +297,12 @@ function Step({ steps, text, code, image, index }: StepProps): JSX.Element {
           initial={{ opacity: 0, x: -50 }}
           whileInView={{ opacity: 1, x: 0 }}
           viewport={{ once: true }}
-          transition={{ duration: 0.5, type: "spring", stiffness: 110, delay: index * 0.30 }}
-        >
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            stiffness: 110,
+            delay: index * 0.3,
+          }}>
           <Image
             src={image}
             alt={text}
@@ -319,47 +339,47 @@ export default function AddYourProjectsGuide(): JSX.Element {
     />
   ));
 
-  return (<>
-    <div className="flex justify-center mt-10">
-      <Motion.button
-        initial={{ opacity: 0, x: -50 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.5, type: "spring", stiffness: 110 }}
-        className={`mx-2 px-4 py-2 rounded-lg mb-4 ${selectedOption === 0
-          ? "bg-primary text-white"
-          : "border border-gray-700 text-gray-300"
+  return (
+    <>
+      <div className="flex justify-center mt-10">
+        <Motion.button
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, type: "spring", stiffness: 110 }}
+          className={`mx-2 px-4 py-2 rounded-lg mb-4 ${
+            selectedOption === 0
+              ? "bg-primary text-white"
+              : "border border-gray-700 text-gray-700 dark:text-gray-300"
           }`}
-        onClick={() => selectOption(0)}
-      >
-        Add Projects Directly From GitHub
-      </Motion.button>
-      <Motion.button
-        initial={{ opacity: 0, x: -50 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.5, type: "spring", stiffness: 110 }}
-
-        className={`mx-2 px-4 py-2 rounded-lg mb-4 ${selectedOption === 1
-          ? "bg-primary text-white"
-          : "border border-gray-700 text-gray-300"
+          onClick={() => selectOption(0)}>
+          Add Projects Directly From GitHub
+        </Motion.button>
+        <Motion.button
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, type: "spring", stiffness: 110 }}
+          className={`mx-2 px-4 py-2 rounded-lg mb-4 ${
+            selectedOption === 1
+              ? "bg-primary text-white"
+              : "border border-gray-700 text-gray-700 dark:text-gray-300"
           }`}
-        onClick={() => selectOption(1)}
-      >
-        Set Up Projects Locally
-      </Motion.button>
-    </div>
-    <article className="items-center max-w-5xl mx-auto">
-      <Motion.p className="mb-5 text-xl font-bold tracking-tight text-white"
-        initial={{ opacity: 0, x: -50 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.5, type: "spring", stiffness: 110 }}
-      >
-        Follow the following steps to add your projects to ProjectsHut :
-      </Motion.p>
-      <div className="option">{stepsRender}</div>
-    </article>
-  </>
+          onClick={() => selectOption(1)}>
+          Set Up Projects Locally
+        </Motion.button>
+      </div>
+      <article className="items-center max-w-5xl mx-auto">
+        <Motion.p
+          className="mb-5 text-xl font-bold tracking-tight text-gray-900 dark:text-white"
+          initial={{ opacity: 0, x: -50 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, type: "spring", stiffness: 110 }}>
+          Follow these steps to add your projects to ProjectsHut:
+        </Motion.p>
+        <div className="option">{stepsRender}</div>
+      </article>
+    </>
   );
 }
